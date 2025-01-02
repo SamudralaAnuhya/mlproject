cb9caed8c84d7503c28aa8b7afc3e20295eaa4ac
SamudralaAnuhya <156963541+SamudralaAnuhya@users.noreply.github.com>
2025-01-01 23:29:57 -0500

    deployement

--- application.py
+++ application.py
@@ -39,4 +39,4 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0" , port=5001)    # it will run on port http://127.0.0.1:5001/predictdata
+    app.run(host="0.0.0.0" , port=5001)    # it will run on port http://127.0.0.1:5001/predictdata (give this link)
